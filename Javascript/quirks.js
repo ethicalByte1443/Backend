441fa197613c6380eb3cf4709d08371b3c7399ce
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-27 23:16:40 +0530

    Adding new learning to js

--- Javascript/quirks.js
+++ Javascript/quirks.js
@@ -8,3 +8,21 @@ console.log(typeof(1.25));              // number
 console.log(typeof(NaN));               // number
 console.log(typeof(null));              // object
 console.log(typeof('a'));               // String
+
+
+console.log(NaN == NaN);                // false
+console.log(NaN === NaN);               // false
+console.log(NaN = NaN);                 // NaN
+
+console.log(.1 + .4);                   // 0.5
+console.log(.1 + .2);                   // 0.30000000000000004
+console.log(.2 + .4);                   // 0.6000000000000001
+console.log([] + []);                   // ''
+
+console.log(1 == "1");                  // true
+console.log(1 === "1");                 // false
+console.log(1 === 1);                   // true
+
+
+
+
