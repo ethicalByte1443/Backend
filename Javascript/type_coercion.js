f0854340336d250e96f02aa3133753b342e69f59
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-28 14:42:13 +0530

    Adding Truthy and falsy and type coercion

--- Javascript/type_coercion.js
+++ Javascript/type_coercion.js
@@ -1,3 +1,17 @@
 // ek type automatically convert ho jayega      
 
-console.log("5" + 1);
+console.log("5" + 1);      // string         // + is concatenation -> number to string
+// always another operand is converted to string if other operand apperas to be string
+
+
+console.log("5" - 1);     // number
+
+
+// **************************************
+//  0 false "" null undefined NaN document.all  => false
+if(null){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
