b95c2aeb7bcb646a4c58919d71f09b4f2eb6ca32
Aseem Pradhan <2k22.cse.2213048@gmail.com>
2025-07-28 16:08:52 +0530

    Adding Truthy and falsy and type coercion

---
+++ Javascript/operators.js
@@ -0,0 +1,32 @@
+// arithmetic, comparison, logical, assignment, unary, ternary
+// + - * / % **
+// = == === != !== >= <= > <
+
+
+console.log(1 + 3);
+console.log(1 + "10");
+console.log("Asee" + "m");
+
+
+
+console.log(12 - 2);
+console.log(12 - "1");
+console.log("Aseem" - "m");             // NaN
+
+
+console.log(2 * 2);
+console.log(2 * "Aseem");               // Nan
+console.log("a" * "s");                 // NaN
+
+
+console.log(2 ** 5);
+
+
+console.log("******* Comparison Operator *******");
+
+
+console.log(12 == 13);          // not strict comparison
+console.log(12 === 12);         // strict comparision
+
+console.log(12 == "12");        // true
+console.log(12 === "12");       /// false
